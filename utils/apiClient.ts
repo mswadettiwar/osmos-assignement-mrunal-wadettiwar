c00e54165a9f59576b4dcea25cbd7560b393323f
mswadettiwar <mrunalwadettiwar07@gmail.com>
2026-02-05 17:31:53 +0530

    Create apiClient.ts

---
+++ utils/apiClient.ts
@@ -0,0 +1,20 @@
+import { APIRequestContext, request } from '@playwright/test';
+
+export class ApiClient {
+  private context!: APIRequestContext;
+
+  async init() {
+    this.context = await request.newContext({
+      baseURL: 'https://reqres.in/api',
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  get(url: string) {
+    return this.context.get(url);
+  }
+
+  post(url: string, body: object) {
+    return this.context.post(url, { data: body });
+  }
+}
